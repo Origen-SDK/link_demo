d969ff3057550ca8763466f8142f5d8c305fad52
Stephen McGinty <stephen.mcginty@freescale.com>
2016-04-22 14:11:06 +0100

    More clean up

--- lib/link_demo/adc_controller.rb
+++ lib/link_demo/adc_controller.rb
@@ -21,16 +21,16 @@ module LinkDemo
       # Ensure some settling time. Default value from initialization or whatever was passed into the options.
       tester.cycle(repeat: opts[:settling_time])
 
-      # Enable the ADC clock, this should really be an API call to the top-level
-      $dut.scgc6.write!(0x4800_0001)
+      ss "Enable the ADC clock" # this should really be an API call to the top-level
+      dut.scgc6.write!(0x4800_0001)
 
-      # Setup the ADC to do 16 averages in hardware
+      ss "Setup the ADC to do 16 averages in hardware"
       sc3.avge.write(0x1) # enable hardware averaging
       sc3.avgs.write(0x2) # take 16 averages
       sc3.avgs.write(0x3) # take 32 averages
       sc3.write!
 
-      # Setup the configuration register
+      ss "Setup the configuration register"
       cfg1.adlpc.write(0x0) # Power Mode
       cfg1.adiv.write(opts[:adc_clock_div]) # Internal ADC Clock Div
       cfg1.adlsmp.write(opts[:long_samples]) # Sample Time (Short Samples = 0, Long Samples = 1)
@@ -38,7 +38,7 @@ module LinkDemo
       cfg1.adiclk.write(0x0) # ADC Input Clock Source
       cfg1.write!
 
-      # Setup the ADC to perform an ADC measurement on the selected channel without differential mode.
+      ss "Perform an ADC measurement on channel #{opts[:channel]} without differential mode"
       sc1.aien.write(0x0) # Ensure conversion-complete interrupt is off
       sc1.diff.write(opts[:diff]) # Write differential mode bit. 0 = Single-ended. 1 = Differential
       sc1.adch.write(opts[:channel]) # Select channel (default channel 24 for sense bus)
@@ -46,16 +46,15 @@ module LinkDemo
 
       # Wait for the conversion. Either 128 clocks per number of conversions or whatever was passed in via
       # the options.
-      $tester.cycle(repeat: opts[:conversion_time])
+      tester.cycle(repeat: opts[:conversion_time])
 
-      # Check that the conversion has completed. The measurements are working but this line still show failures...
-      # Not sure why, need to look into this. Possibly an error with the block being updated and the RGen register
-      # model doesn't know about it.
+      ss "Check that the conversion has completed"
       sc1.coco.read!(0x1) # Check for COnversion COmplete (COCO)
 
-      # Return the register containing the result. Digital measurement, convert using standard D/A conversion,
+      # Return the register bits containing the result to the caller.
+      # Digital measurement, convert using standard D/A conversion,
       # Vref ~= 3.3 V for default reference
-      r
+      r.d
     end
   end
 end
